a2183784e6d05ea900d06b46e529fd2e615033fc
simonbhh <bouhbouh300@gmail.com>
2020-01-13 19:00:39 +0100

    Adding props to the DashboardTable component

--- app/pages/dashboard/DashboardTable.jsx
+++ app/pages/dashboard/DashboardTable.jsx
@@ -9,61 +9,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
-const columns = [
-    { id: 'user', label: 'User', minWidth: 100 },
-    {
-        id: 'sub_task1',
-        label: 'Score sub-task 1',
-        minWidth: 60,
-        align: 'right',
-        format: value => value.toLocaleString(),
-    },
-    {
-        id: 'sub_task2',
-        label: 'Score sub-task 2',
-        minWidth: 60,
-        align: 'right',
-        format: value => value.toLocaleString(),
-    },
-    {
-        id: 'sub_task3',
-        label: 'Score sub-task 3',
-        minWidth: 60,
-        align: 'right',
-        format: value => value.toLocaleString(),
-    },
-    {
-        id: 'task_score',
-        label: 'Task average score',
-        minWidth: 60,
-        align: 'right',
-        format: value => value.toLocaleString(),
-    }
-];
-
-function createData(user, sub_task1, sub_task2, sub_task3) {
-    const task_score = (sub_task1 + sub_task2 + sub_task3) / 3;
-    return { user, sub_task1, sub_task2, sub_task3, task_score };
-}
-
-const rows = [
-    createData('Jean', 7, 8.5, 5.6),
-    createData('Paul', 7, 8.5, 5.6),
-    createData('Pena', 7, 8.5, 5.6),
-    createData('Pierre', 7, 8.5, 5.6),
-    createData('Jacques', 7, 8.5, 5.6),
-    createData('Thomas', 7, 8.5, 5.6),
-    createData('Rémi', 7, 8.5, 5.6),
-    createData('Alexandre', 7, 8.5, 5.6),
-    createData('Jeanne', 7, 8.5, 5.6),
-    createData('Estelle', 7, 8.5, 5.6),
-    createData('Solène', 7, 8.5, 5.6),
-    createData('Louise', 7, 8.5, 5.6),
-    createData('Marie', 7, 8.5, 5.6),
-    createData('Robert', 7, 8.5, 5.6),
-    createData('Lola', 7, 8.5, 5.6),
-];
-
 const useStyles = makeStyles({
     root: {
         width: '100%',
@@ -74,6 +19,9 @@ const useStyles = makeStyles({
 });
 
 function DashboardTable(props) {
+
+    const rows = props.rows
+    const columns = props.columns
 
     const classes = useStyles();
     const [page, setPage] = useState(0);
@@ -89,50 +37,52 @@ function DashboardTable(props) {
     };
 
     return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map(column => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
+        <div>
+            <Paper className={classes.root}>
+              <TableContainer className={classes.container}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      {columns.map(column => (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{ minWidth: column.minWidth }}
+                        >
+                          {column.label}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                      return (
+                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                          {columns.map(column => {
+                            const value = row[column.id];
+                            return (
+                              <TableCell key={column.id} align={column.align}>
+                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                              </TableCell>
+                            );
+                          })}
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+              />
+            </Paper>
+        </div>
     );
 
 }
--- app/pages/dashboard/project.jsx
+++ app/pages/dashboard/project.jsx
@@ -2,11 +2,69 @@
 import React, {useState, useEffect} from 'react'
 import DashboardTable from 'DashboardTable'
 
+function createData(user, sub_task1, sub_task2, sub_task3) {
+    const task_score = (sub_task1 + sub_task2 + sub_task3) / 3;
+    return { user, sub_task1, sub_task2, sub_task3, task_score };
+}
+
+const rows = [
+    createData('Jean', 7, 8.5, 5.6),
+    createData('Paul', 7, 8.5, 5.6),
+    createData('Pena', 7, 8.5, 5.6),
+    createData('Pierre', 7, 8.5, 5.6),
+    createData('Jacques', 7, 8.5, 5.6),
+    createData('Thomas', 7, 8.5, 5.6),
+    createData('Rémi', 7, 8.5, 5.6),
+    createData('Alexandre', 7, 8.5, 5.6),
+    createData('Jeanne', 7, 8.5, 5.6),
+    createData('Estelle', 7, 8.5, 5.6),
+    createData('Solène', 7, 8.5, 5.6),
+    createData('Louise', 7, 8.5, 5.6),
+    createData('Marie', 7, 8.5, 5.6),
+    createData('Robert', 7, 8.5, 5.6),
+    createData('Lola', 7, 8.5, 5.6),
+];
+
+const columns = [
+    { id: 'user', label: 'User', minWidth: 100 },
+    {
+        id: 'sub_task1',
+        label: 'Score sub-task 1',
+        minWidth: 60,
+        align: 'right',
+        format: value => value.toLocaleString(),
+    },
+    {
+        id: 'sub_task2',
+        label: 'Score sub-task 2',
+        minWidth: 60,
+        align: 'right',
+        format: value => value.toLocaleString(),
+    },
+    {
+        id: 'sub_task3',
+        label: 'Score sub-task 3',
+        minWidth: 60,
+        align: 'right',
+        format: value => value.toLocaleString(),
+    },
+    {
+        id: 'task_score',
+        label: 'Task average score',
+        minWidth: 60,
+        align: 'right',
+        format: value => value.toLocaleString(),
+    }
+];
+
 function DashboardPageProject() {
     return (
         <div>
-            <p>Ceci est la page Project</p>
-            <DashboardTable />
+            <p>This is the project page</p>
+            <DashboardTable
+                rows = {rows}
+                columns = {columns}
+            />
         </div>
     );
 }
