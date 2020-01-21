73eb8e5c2440621c81077dc7b63f68a9e18ad66d
Antoine Marjault <antoine.marjault@gmail.com>
2020-01-21 18:50:17 +0100

    Added utils.js

--- app/pages/dashboard/general.jsx
+++ app/pages/dashboard/general.jsx
@@ -2,95 +2,83 @@ import React from 'react';
 import apiClient from 'panoptes-client/lib/api-client';
 import { Link, IndexLink } from 'react-router';
 import ProjectList from './ProjectList'
+import * as utils from './utils'
 
 class DashboardPageGeneral extends React.Component {
-    state={
-        loaded: false,
-        projects: []
+  state={
+    loaded: false,
+    projects: []
+  }
+
+  componentDidMount() {
+    this.loadProjects()
+    //this.getAllUsers()
+    //this.getClassifications(604)
+  }
+
+  loadProjects() {
+    const query = {
+      tags: undefined,
+      sort: "-launch_date",
+      launch_approved: true,
+      cards: true,
+      include: ['avatar'],
+      state: 'live',
+      page_size: 10000,
     }
 
-    componentDidMount() {
-      this.loadProjects()
-      //this.getMyProject()
-      //this.getAllUsers()
-      //this.getClassifications(604)
-    }
-
-    loadProjects() {
-        const query = {
-            tags: undefined,
-            sort: "-launch_date",
-            launch_approved: true,
-            cards: true,
-            include: ['avatar'],
-            state: 'live',
-            page_size: 10000,
-        }
-
-        apiClient.type('projects').get(query)
-            .then((projects) => {
-                if (projects.length > 0) {
-                  this.setState({ projects })
-                  this.setState({ loaded: true })
-                } else {
-                  this.setState({ projects: []});
-                }
-            })
-    }
-
-/*    getMyProject() {
-      apiClient.type('subjects').get('1899')
-        .then(function (subject) {
-            console.log('project 1:' + subject);
-        });
-
-      apiClient.type('projects').get( {id: '433' })
-          .then((projects) => {
-            console.log('project : ' + projects)
-          })
-    }
-*/
-    getClassifications(project_id) {
-      apiClient.type('classifications').get(1)
-          .then((classif) => {
-            console.log(classif)
-          })
-    }
-
-    getAllUsers() {
-
-      const query = {
-        page: 132378, //=> 157245
-        page_size: 10, // 1326029
-        sort: '-login'
+    utils.getProjects(query)
+    .then((projects) => {
+      if (projects.length > 0) {
+        this.setState({ projects, loaded:true })
+      } else {
+        this.setState({ projects: []});
       }
-      //132378
-      //{ id: '1326029' }
-      //{ id: '1325316'} => {login: 'markb-panoptes' }
+    })
+  }
 
-      apiClient.type('users').get({ id: '1326029' })
-        .then(function (users) {
-            console.log(users);
-        });
+
+  getClassifications(project_id) {
+    apiClient.type('classifications').get(1)
+    .then((classif) => {
+      console.log(classif)
+    })
+  }
+
+  getAllUsers() {
+
+    const query = {
+      page: 132378, //=> 157245
+      page_size: 10, // 1326029
+      sort: '-login'
     }
+    //132378
+    //{ id: '1326029' }
+    //{ id: '1325316'} => {login: 'markb-panoptes' }
+
+    apiClient.type('users').get({ id: '1326029' })
+    .then(function (users) {
+      console.log(users);
+    });
+  }
 
 
   render() {
     const projects = this.state.loaded ? <ProjectList projects={this.state.projects} /> : "loading ..."
     return (
       <div>
-        <h1>Liste des projets</h1>
-        <p> Sélectionner un projet pour accéder à ses statistiques : </p>
-        <ul>
-            {projects}
-        </ul>
+      <h1>Liste des projets</h1>
+      <p> Sélectionner un projet pour accéder à ses statistiques : </p>
+      <ul>
+      {projects}
+      </ul>
       </div>
     );
   }
 }
 
 /*
-    Créer une liste de projets avec leur nombre de classification à côté
+Créer une liste de projets avec leur nombre de classification à côté
 */
 
 export default DashboardPageGeneral
---
+++ app/pages/dashboard/utils.js
@@ -0,0 +1,18 @@
+import apiClient from 'panoptes-client/lib/api-client';
+
+/**
+* Fetch projects from panoptes API
+* Can fetch a single project by querying getProjects(project_id)
+* @param {object} query Query parameters (see Panoptes API documentation)
+* @returns {promise}
+*/
+export function getProjects(query) {
+  return new Promise(function(resolve, reject) {
+    resolve(
+      apiClient.type('projects').get(query)
+      .then((projects) => {
+        return projects;
+      })
+    )
+  })
+}
